14bb417f9e1d76ff363b5243b911f0dd27423fcf
Karl Swedberg <kswedberg@gmail.com>
2014-02-09 12:27:25 -0500

    Add uglify and jscs to init npm installs

--- init/npm.sh
+++ init/npm.sh
@@ -1,7 +1,9 @@
 npm install -g bower
 npm install -g grunt-cli
 npm install -g yo
-# npm install -g uglify-js
+npm install -g uglify-js
+npm install -g jscs
+
 # npm install -g generator-generator
 # npm install -g generator-webapp
 # npm install -g generator-mobile
