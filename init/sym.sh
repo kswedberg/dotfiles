164ab1abea2e5228af118c5df75ea726db1665bb
Karl Swedberg <kswedberg@gmail.com>
2016-03-19 13:06:05 -0400

    Remove init/sym.sh in favor of more complete setup.sh

--- init/sym.sh
+++
@@ -1,7 +0,0 @@
-ln -s $DOTFILES/.atom $HOME/.atom
-ln -s $DOTFILES/.jscsrc $HOME/.jscsrc
-ln -s $DOTFILES/.jshintrc $HOME/.jshintrc
-ln -s $DOTFILES/.profile $HOME/.profile
-ln -s $DOTFILES/.slate $HOME/.slate
-ln -s $DOTFILES/.zshrc $HOME/.zshrc
-ln -s $DOTFILES/.zprofile $HOME/.zprofile
