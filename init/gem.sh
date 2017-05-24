e525c1a89e6146aea5f993b8e91a078d9dd8ebfd
Karl Swedberg <kswedberg@gmail.com>
2017-05-24 09:02:27 -0400

    Add ssh server tunnel for (used for access to remote RethinkDB web admin)

--- init/gem.sh
+++ init/gem.sh
@@ -1,7 +1,7 @@
 gem install bundler
 gem install sass
 gem install compass
-gem install pygmentize
 gem install git_remote_branch
 gem install rubygems-bundler
 gem regenerate_binstubs
+gem install pygmentize
