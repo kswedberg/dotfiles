34e0a1dba0bce4b5a7c7b42f019f4e91acfedd2a
Karl Swedberg <kswedberg@gmail.com>
2017-09-25 10:51:59 -0400

    gem install pygmentize before regenerate_ginstubs

--- init/gem.sh
+++ init/gem.sh
@@ -2,6 +2,6 @@ gem install bundler
 gem install sass
 gem install compass
 gem install git_remote_branch
+gem install pygmentize
 gem install rubygems-bundler
 gem regenerate_binstubs
-gem install pygmentize
