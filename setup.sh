804fa4ce861427f8eba5514863f0874a99cfd53c
Karl Swedberg <kswedberg@gmail.com>
2017-12-01 10:17:23 -0500

    Update Brewfie and setup.sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,11 @@ DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 red=`tput setaf 1`
 reset=`tput sgr0`
 
+#### HOMEBREW
 # Install Homebrew if not already there
 if [[ ! -f /usr/local/bin/brew ]]; then
   echo "Homebrew does not exist. Installing…"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Install nvm if not already there
-if [[ ! -d ~/.nvm ]]; then
-  echo ".nvm does not exist. Installing…"
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-fi
-
-# Install rbenv if not already there
-if [[ ! -d ~/.rbenv ]]; then
-  echo ".rbenv does not exist. Installing…"
-  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 fi
 
 # Update Homebrew recipes
@@ -35,14 +24,31 @@ source $DOTFILES/init/cask.sh
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Install global Composer packages
-composer global require laravel/installer
+source ~/.zshrc
+
+# Install nvm if not already there
+if [[ ! -d ~/.nvm ]]; then
+  echo ".nvm does not exist. Installing…"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+fi
+
+# Install latest node.js
+nvm install node
 
 # Install global npm packages
 source $DOTFILES/init/npm.sh
 
+# Install a "recent" Ruby version (as of 2017-12-01) and use it
+rbenv install 2.4.2
+rbenv global 2.4.2
+
 # Install Ruby Gems
 source $DOTFILES/init/gem.sh
 
+# Install global Composer packages
+composer global require laravel/installer
+
 # Set some MacOS defaults
 source $DOTFILES/init/macos.sh
+
+echo "All done!"
