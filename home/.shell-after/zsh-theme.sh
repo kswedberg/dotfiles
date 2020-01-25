4b1dec99b276f926e04fa81144e5d680acdf38d3
Karl Swedberg <kswedberg@gmail.com>
2020-01-25 11:24:38 -0500

    Update zsh-theme to avoid errors when no virtualenv_prompt_info or git_prompt_status

--- home/.shell-after/zsh-theme.sh
+++ home/.shell-after/zsh-theme.sh
@@ -1,27 +1,31 @@
-
 # My elements:
 KARL_CURRENT_TIME_="%{$fg[white]%}%{$fg[yellow]%}%W %t%{$fg[white]%}%{$reset_color%}"
 KARL_OPEN_ANGLE="%{$fg[white]%}‹%{$fg[red]%}"
 KARL_CLOSE_ANGLE="%{$fg[white]%}›%{$reset_color%}"
 
-function karl_precmd {
+function karl_precmd() {
   KARL_PRE_STATUS=""
   KARL_POST_STATUS=""
-  gitstatus=$(git_prompt_status)
-  if [ ${#gitstatus} != 0 ]; then
-    KARL_PRE_STATUS="( "
-    KARL_POST_STATUS=" %{$reset_color%})"
-  fi
-
   KARL_POST_GIT="%{$reset_color%}"
-  git=$(git_prompt_info)
-  if [ ${#git} != 0 ]; then
-    KARL_POST_GIT=" %{$reset_color%}
+  karl_virtualenv=""
+
+  if which git_prompt_status &>/dev/null; then
+    gitstatus=$(git_prompt_status)
+    if [ ${#gitstatus} != 0 ]; then
+      KARL_PRE_STATUS="( "
+      KARL_POST_STATUS=" %{$reset_color%})"
+    fi
+
+    git=$(git_prompt_info)
+    if [ ${#git} != 0 ]; then
+      KARL_POST_GIT=" %{$reset_color%}
 "
+    fi
   fi
 
-  karl_virtualenv=$(virtualenv_prompt_info)
-
+  if which virtualenv_prompt_info &>/dev/null; then
+    karl_virtualenv=$(virtualenv_prompt_info)
+  fi
   # local IN_RMOUNT_PATH=`echo "${PWD}" | grep "${RMOUNT_DIR}"`
   PATHCOLOR="%{$reset_color%}"
   # if [[ -n "${IN_RMOUNT_PATH}" ]]; then
@@ -43,14 +47,13 @@ local user_host='%{$terminfo[bold]$fg[green]%}%n@%m%{$reset_color%}'
 current_dir='%~'
 
 local rvm_ruby=''
-if which rbenv &> /dev/null; then
- rvm_ruby="\${\$(rbenv version | sed -e 's/ (set.*$//' -e 's/^ruby-//')}"
+if which rbenv &>/dev/null; then
+  rvm_ruby="\${\$(rbenv version | sed -e 's/ (set.*$//' -e 's/^ruby-//')}"
 else
-  if which rvm-prompt &> /dev/null; then
+  if which rvm-prompt &>/dev/null; then
     rvm_ruby='%{$fg[red]%}$(rvm-prompt i v g)%{$reset_color%}'
   fi
 fi
-
 
 ZSH_THEME_GIT_PROMPT_PREFIX="[%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="] %{$reset_color%}"
