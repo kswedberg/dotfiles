a1c080482ccce825fd1088d40dcd69ce3ccb5426
Karl Swedberg <kswedberg@gmail.com>
2020-02-08 13:01:31 -0500

    Fix backup.sh, run using bash

--- backup.sh
+++ backup.sh
@@ -1,15 +1,19 @@
-#!/usr/bin/env zsh
-declare -a backup_cmds=("brew bundle dump --file=$DOTFILES/Brewfile --force"
-  "mackup backup --force")
+#!/usr/bin/env bash
+declare -a backup_cmds=(
+  "brew bundle dump --file=$DOTFILES/Brewfile --force"
+  "mackup backup --force"
+)
 
-echo "\nYou are about to back up your settings and files."
-echo "\nThis will run the following commands:"
+echo "You are about to back up your settings and files."
+echo "This will run the following commands:"
 
 for ((i = 0; i < ${#backup_cmds[@]}; i++)); do
   printf   "\n* ${backup_cmds[$i]}"
 done
 
-echo "\nAre you sure? [y/N]"
+echo -e "\n\n"
+
+echo "Are you sure? [y/N]"
 read CONFIRM
 
 if [ "$CONFIRM" = "y" ]; then
@@ -27,10 +31,10 @@ if [ "$CONFIRM" = "y" ]; then
   done
 
 else
-  echo "Okay, not gonna do it."
+  echo -e "\nOkay, not gonna do it."
 fi
 
-echo "\nWant to run bundle-ignored to put all your secret files from this project into secrets.tar.gz? [y/N]"
+echo -e "\nWant to run bundle-ignored to put all your secret files from this project into secrets.tar.gz? [y/N]"
 read CONFIRM_BUNDLE
 
 if [ "$CONFIRM_BUNDLE" = "y" ]; then
@@ -38,7 +42,7 @@ if [ "$CONFIRM_BUNDLE" = "y" ]; then
   $(./fn/bundle-ignored secrets)
 
   echo "(Ignore the warning about no such file or directory if you see it)"
-  echo "\nFinished!"
+  echo -e "\nFinished!"
 else
-  echo "Okay, bye!"
+  echo -e "\nOkay, bye!"
 fi
