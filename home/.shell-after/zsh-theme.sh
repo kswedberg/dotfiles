32f0891c3c2d100ac99cade0ceeb1924ca304bdc
Karl Swedberg <kswedberg@gmail.com>
2022-01-17 09:09:35 -0500

    Lint indentation for zsh-theme

--- home/.shell-after/zsh-theme.sh
+++ home/.shell-after/zsh-theme.sh
@@ -33,52 +33,55 @@ function karl_precmd() {
   # fi
 }
 
-# Remove `add-zsh-hook precmd karl_precmd` in favor of `precmd_function+=(...)`
-# cf https://github.com/robbyrussell/oh-my-zsh/issues/748#issuecomment-37862691
-precmd_functions+=(karl_precmd)
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
 
-if [[ $USER == "root" ]]; then
-  CARETCOLOR="red"
-else
-  CARETCOLOR="white"
-fi
+  # Remove `add-zsh-hook precmd karl_precmd` in favor of `precmd_function+=(...)`
+  # cf https://github.com/robbyrussell/oh-my-zsh/issues/748#issuecomment-37862691
+  precmd_functions+=(karl_precmd)
 
-local user_host='%{$terminfo[bold]$fg[green]%}%n@%m%{$reset_color%}'
-current_dir='%~'
-
-local rvm_ruby=''
-if which rbenv &>/dev/null; then
-  rvm_ruby="\${\$(rbenv version | sed -e 's/ (set.*$//' -e 's/^ruby-//')}"
-else
-  if which rvm-prompt &>/dev/null; then
-    rvm_ruby='%{$fg[red]%}$(rvm-prompt i v g)%{$reset_color%}'
+  if [[ $USER == "root" ]]; then
+    CARETCOLOR="red"
+  else
+    CARETCOLOR="white"
   fi
+
+  local user_host='%{$terminfo[bold]$fg[green]%}%n@%m%{$reset_color%}'
+  current_dir='%~'
+
+  local rvm_ruby=''
+  if which rbenv &>/dev/null; then
+    rvm_ruby="\${\$(rbenv version | sed -e 's/ (set.*$//' -e 's/^ruby-//')}"
+  else
+    if which rvm-prompt &>/dev/null; then
+      rvm_ruby='%{$fg[red]%}$(rvm-prompt i v g)%{$reset_color%}'
+    fi
+  fi
+
+  ZSH_THEME_GIT_PROMPT_PREFIX="[%{$reset_color%}"
+  ZSH_THEME_GIT_PROMPT_SUFFIX="] %{$reset_color%}"
+
+  ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗%{$reset_color%}"
+  ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}✔%{$reset_color%}"
+
+  ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}✚"
+  ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[cyan]%}⇡"
+  ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[cyan]%}⇣"
+  ZSH_THEME_GIT_PROMPT_DIVERGED="%{$fg[yellow]%}⚑"
+
+  # local KS_VIRTUAL_ENV_PROMPT = ''
+  # if [[ -n ${VIRTUAL_ENV} ]]; then
+  #   KS_VIRTUAL_ENV_PROMPT = ''
+  # else
+  #   KS_VIRTUAL_ENV_PROMPT = "${ZSH_THEME_VIRTUALENV_PREFIX:=[}${VIRTUAL_ENV:t}${ZSH_THEME_VIRTUALENV_SUFFIX:=]}"
+  # fi
+
+  NODE_ICON=$'\U2B22'
+  RUBY_ICON=$'\U2B18'
+
+  PROMPT='
+  ${KARL_CURRENT_TIME_} ${karl_virtualenv} ${KARL_OPEN_ANGLE}${RUBY_ICON} '"${rvm_ruby}"'${KARL_CLOSE_ANGLE} [%{$fg[green]%}${NODE_ICON} `node -v`%{$reset_color%}]${PATHCOLOR} ${current_dir}
+  %{$reset_color%}$(git_super_status)${KARL_POST_GIT}%{${fg[$CARETCOLOR]}%}❯ %{$reset_color%}'
+  # $(git_prompt_info)${KARL_PRE_STATUS}$(git_prompt_status)${KARL_POST_STATUS}${KARL_POST_GIT}%{${fg[$CARETCOLOR]}%}❯ %{${reset_color}%}'
+  # RPROMPT='[`node -v`]'
+  RPROMPT=''
 fi
-
-ZSH_THEME_GIT_PROMPT_PREFIX="[%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="] %{$reset_color%}"
-
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}✔%{$reset_color%}"
-
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}✚"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[cyan]%}⇡"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[cyan]%}⇣"
-ZSH_THEME_GIT_PROMPT_DIVERGED="%{$fg[yellow]%}⚑"
-
-# local KS_VIRTUAL_ENV_PROMPT = ''
-# if [[ -n ${VIRTUAL_ENV} ]]; then
-#   KS_VIRTUAL_ENV_PROMPT = ''
-# else
-#   KS_VIRTUAL_ENV_PROMPT = "${ZSH_THEME_VIRTUALENV_PREFIX:=[}${VIRTUAL_ENV:t}${ZSH_THEME_VIRTUALENV_SUFFIX:=]}"
-# fi
-
-NODE_ICON=$'\U2B22'
-RUBY_ICON=$'\U2B18'
-
-PROMPT='
-${KARL_CURRENT_TIME_} ${karl_virtualenv} ${KARL_OPEN_ANGLE}${RUBY_ICON} '"${rvm_ruby}"'${KARL_CLOSE_ANGLE} [%{$fg[green]%}${NODE_ICON} `node -v`%{$reset_color%}]${PATHCOLOR} ${current_dir}
-%{$reset_color%}$(git_super_status)${KARL_POST_GIT}%{${fg[$CARETCOLOR]}%}❯ %{$reset_color%}'
-# $(git_prompt_info)${KARL_PRE_STATUS}$(git_prompt_status)${KARL_POST_STATUS}${KARL_POST_GIT}%{${fg[$CARETCOLOR]}%}❯ %{${reset_color}%}'
-# RPROMPT='[`node -v`]'
-RPROMPT=''
