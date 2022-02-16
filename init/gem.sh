8a969cda9f1c0e76e76247d68cb865c4c933ac60
Karl Swedberg <kswedberg@gmail.com>
2022-02-16 12:21:50 -0500

    Update gems

--- init/gem.sh
+++ init/gem.sh
@@ -3,5 +3,6 @@ gem install sass
 # gem install compass
 # gem install git_remote_branch
 gem install pygmentize
+gem install rubocop
 gem install rubygems-bundler
-gem regenerate_binstubs
+# gem regenerate_binstubs
