11a11f21e94a0212e540c7aa21d994cd21d2ba5d
Karl Swedberg <kswedberg@gmail.com>
2025-12-13 11:26:01 -0500

    Don't disable press-and-hold for alt keys (use these now for for diacritics)

--- init/macos.sh
+++ init/macos.sh
@@ -76,7 +76,8 @@ if [ "$CONFIRMMACOS" = "y" ]; then
   # defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
   # Disable press-and-hold for keys in favor of key repeat
-  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  # defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
 
   # Set a fast keyboard repeat rate
   defaults write NSGlobalDomain KeyRepeat -int 1
